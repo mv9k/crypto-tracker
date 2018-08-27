7096fb785d0219ba90656508a3c0b9cbf1f84d43
Kris <mv9kjw@gmail.com>
2018-08-27 01:36:46 -0600

    layout

--- src/App.js
+++ src/App.js
@@ -1,34 +1,40 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import { Button, Row, Col } from 'antd';
+import { Layout, Menu, Breadcrumb } from 'antd';
 import './App.css';
+
+const { Header, Content, Footer } = Layout;
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <Button type="primary">Button</Button>
-          <Row gutter={16}>
-              <Col className="gutter-row" span={6}>
-                  <div className="gutter-box">col-6</div>
-              </Col>
-              <Col className="gutter-row" span={6}>
-                  <div className="gutter-box">col-6</div>
-              </Col>
-              <Col className="gutter-row" span={6}>
-                  <div className="gutter-box">col-6</div>
-              </Col>
-              <Col className="gutter-row" span={6}>
-                  <div className="gutter-box">col-6</div>
-              </Col>
-          </Row>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          <Layout className="layout">
+              <Header>
+                  <div className="logo" />
+                  <Menu
+                      theme="dark"
+                      mode="horizontal"
+                      defaultSelectedKeys={['2']}
+                      style={{ lineHeight: '64px' }}
+                  >
+                      <Menu.Item key="1">nav 1</Menu.Item>
+                      <Menu.Item key="2">nav 2</Menu.Item>
+                      <Menu.Item key="3">nav 3</Menu.Item>
+                  </Menu>
+              </Header>
+              <Content style={{ padding: '0 50px' }}>
+                  <Breadcrumb style={{ margin: '16px 0' }}>
+                      <Breadcrumb.Item>Home</Breadcrumb.Item>
+                      <Breadcrumb.Item>List</Breadcrumb.Item>
+                      <Breadcrumb.Item>App</Breadcrumb.Item>
+                  </Breadcrumb>
+                  <div style={{ background: '#fff', padding: 24, minHeight: 280 }}>Content</div>
+              </Content>
+              <Footer style={{ textAlign: 'center' }}>
+                  Crypto Tracker ©2018 Created by Kris W.
+              </Footer>
+          </Layout>
       </div>
     );
   }
